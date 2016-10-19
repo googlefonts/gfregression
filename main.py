72a99c11c5eab7f7c009d8de5255a5efa6894e19
Marc Foley <m.foley.88@gmail.com>
2016-10-19 19:54:01 +0200

    main.py: pepify

--- main.py
+++ main.py
@@ -48,21 +48,34 @@ def fonts_on_google(local_fonts):
         api_fam_name = re.sub('(?!^)([A-Z]+)', r'+\1', fam_name)
         css_fam_name = re.sub('(?!^)([A-Z]+)', r' \1', fam_name)
         url = '%s%s:%s' % (url_prefix, api_fam_name, FONT_WEIGHTS[style])
-        if requests.get(url).status_code == 200:
+        if requests.get(url).status_code == 200:  # Check if font exists
             if 'i' in FONT_WEIGHTS[style]:
-                fonts.append((fam_name, css_fam_name, FONT_WEIGHTS[style][:-1], 'italic', url))
+                fonts.append((fam_name,
+                              css_fam_name,
+                              FONT_WEIGHTS[style][:-1],
+                              'italic',
+                              url))
             else:
-                fonts.append((fam_name, css_fam_name, FONT_WEIGHTS[style], 'normal', url))
+                fonts.append((fam_name,
+                              css_fam_name,
+                              FONT_WEIGHTS[style],
+                              'normal',
+                              url))
         else:
-            fonts.append(('Inconsolata', 'Inconsolata', '400', 'normal', fallback_font))
+            fonts.append(('Inconsolata',
+                          'Inconsolata',
+                          '400',
+                          'normal',
+                          fallback_font))
     return fonts
 
 
 @app.route("/")
 def test_fonts():
     sep = os.path.sep
+    # web browsers use unix slashes, instead of windows
     local_fonts = [(p.replace('\\', '/'), ntpath.basename(p)[:-4])
-        for p in glob.glob("."+ sep + "static" + sep + "*.ttf")]
+                   for p in glob.glob("." + sep + "static" + sep + "*.ttf")]
     google_fonts = fonts_on_google(local_fonts)
     print google_fonts
     return render_template('index.html',

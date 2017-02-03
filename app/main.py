4b23079b782bfb6f78a1a442b530564edef35a1f
Marc Foley <m.foley.88@gmail.com>
2017-02-03 15:49:44 +0000

    v0.100 update.
    Comparisons added for the following:
    - Vertical metrics
    - Modified glyphs

--- app/main.py
+++ app/main.py
@@ -18,6 +18,8 @@ from zipfile import ZipFile
 from StringIO import StringIO
 from pprint import pprint
 
+__version__ = 0.100
+
 URL_PREFIX = 'https://fonts.google.com/download?family='
 
 FONT_EXCEPTIONS = [
@@ -119,18 +121,18 @@ def css_property(path, fullname, cssname, font):
     return font
 
 
-def inconsistent_fonts_glyphs(local_fonts, remote_fonts, names):
+def inconsistent_fonts_glyphs(local_fonts, remote_fonts):
     """return glyphs which have changed from local to remote"""
     glyphs = {}
     bad_glyphs = {}
 
-    for l_font, r_font, font_name in zip(local_fonts, remote_fonts, names):
-        l_glyphs = l_font['glyf'].glyphs.keys()
-        r_glyphs = r_font['glyf'].glyphs.keys()
+    for font in local_fonts:
+        l_glyphs = local_fonts[font].font['glyf'].glyphs.keys()
+        r_glyphs = remote_fonts[font].font['glyf'].glyphs.keys()
         shared_glyphs = set(l_glyphs) & set(r_glyphs)
 
-        l_glyphs = l_font.getGlyphSet()
-        r_glyphs = r_font.getGlyphSet()
+        l_glyphs = local_fonts[font].font.getGlyphSet()
+        r_glyphs = remote_fonts[font].font.getGlyphSet()
 
         l_pen = AreaPen(l_glyphs)
         r_pen = AreaPen(r_glyphs)
@@ -147,16 +149,16 @@ def inconsistent_fonts_glyphs(local_fonts, remote_fonts, names):
             if l_area != r_area:
                 if int(l_area) ^ int(r_area) > GLYPH_THRESHOLD:
 
-                    if font_name not in bad_glyphs:
-                        bad_glyphs[font_name] = []
-                    bad_glyphs[font_name].append(glyph)
+                    if font not in bad_glyphs:
+                        bad_glyphs[font] = []
+                    bad_glyphs[font].append(glyph)
                     print('Different %s' % glyph)
 
-        l_cmap_tbl = l_font['cmap'].getcmap(3, 1).cmap
+        l_cmap_tbl = local_fonts[font].font['cmap'].getcmap(3, 1).cmap
         try:
-            glyphs[font_name] = [i for i in l_cmap_tbl.items() if i[1] in bad_glyphs[font_name]]
+            glyphs[font] = [i for i in l_cmap_tbl.items() if i[1] in bad_glyphs[font]]
         except:
-            print('%s has consistent glyphs' % font_name)
+            print('%s has consistent glyphs' % font)
     return glyphs
 
 
@@ -174,45 +176,37 @@ def test_fonts():
 
     local_fonts_paths = glob(LOCAL_FONTS_PATH + '*.ttf')
     local_fonts = fonts(local_fonts_paths, 'new')
-    local_fonts = sorted(local_fonts, key=lambda x: x.fullname)
 
     # Assemble download url for families
     remote_download_url = gf_download_url([i.fullname for i in local_fonts])
     # download last fonts from fonts.google.com
     if url_200_response(remote_download_url):
         remote_fonts_zip = download_family_from_gf(remote_download_url)
-
         fonts_from_zip(remote_fonts_zip, REMOTE_FONTS_PATH)
 
         remote_fonts_paths = glob(REMOTE_FONTS_PATH + '*.ttf')
         remote_fonts = fonts(remote_fonts_paths, 'old')
-        remote_fonts = sorted(remote_fonts, key=lambda x: x.fullname)
     else:
-        remote_fonts = []
+        return 'Font is not hosted on fonts.google.com'
 
-    remote_fonts_names = [f.fullname for f in remote_fonts]
-    local_fonts_names = [f.fullname for f in local_fonts]
+    shared_fonts = set([i.fullname for i in local_fonts]) & \
+                   set([i.fullname for i in remote_fonts])
 
-    local_fonts = [f for f in local_fonts if f.fullname in remote_fonts_names]
-    remote_fonts = [f for f in remote_fonts if f.fullname in local_fonts_names]
-    local_fonts = sorted(local_fonts, key=lambda x: x.fullname)
-    remote_fonts = sorted(remote_fonts, key=lambda x: x.fullname)
-    print(zip([f.path for f in local_fonts], [f.path for f in remote_fonts]))
-    char_maps = inconsistent_fonts_glyphs(
-        [f.font for f in local_fonts],
-        [f.font for f in remote_fonts],
-        [f.fullname for f in local_fonts],
-    )
-    # print(pprint(char_maps))
+    local_fonts = {f.fullname: f for f in local_fonts
+                   if f.fullname in shared_fonts}
+    remote_fonts = {f.fullname: f for f in remote_fonts
+                    if f.fullname in shared_fonts}
 
-    to_local_fonts = ','.join([i.cssname for i in local_fonts])
-    to_remote_fonts = ','.join([i.cssname for i in remote_fonts])
+    changed_glyphs = inconsistent_fonts_glyphs(local_fonts, remote_fonts)
+
+    to_local_fonts = ','.join([local_fonts[i].cssname for i in local_fonts])
+    to_remote_fonts = ','.join([remote_fonts[i].cssname for i in remote_fonts])
     return render_template(
         'index.html',
         dummy_text=dummy_text,
-        local_fonts=local_fonts,
-        remote_fonts=remote_fonts,
-        char_maps=char_maps,
+        local_fonts=local_fonts.values(),
+        remote_fonts=remote_fonts.values(),
+        changed_glyphs=changed_glyphs,
         to_local_fonts=to_local_fonts,
         to_remote_fonts=to_remote_fonts
     )

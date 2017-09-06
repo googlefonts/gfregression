32cc32cf0454fbb90909c394f70df65e48c4491e
Marc Foley <m.foley.88@gmail.com>
2017-09-06 14:38:09 +0100

    Ignore languages if no GSUB table exists

--- app/comparefonts.py
+++ app/comparefonts.py
@@ -91,22 +91,26 @@ class CompareFonts:
         sample text"""
         font_languages = {}
         for font in self.target_fonts:
-            script_records = self.target_fonts[font].font['GSUB'].table.ScriptList.ScriptRecord
+            try:
+                script_records = self.target_fonts[font].font['GSUB'].table.ScriptList.ScriptRecord
 
-            font_languages[font] = {}
-            for script in script_records:
-                font_languages[font][script2iso[script.ScriptTag]] = ''
-                languages = list(script.Script.LangSysRecord)
-                for language in languages:
-                    lang_tag = language.LangSysTag
-                    font_languages[font][lang_tag] = ''
+                font_languages[font] = {}
+                for script in script_records:
+                    font_languages[font][script2iso[script.ScriptTag]] = ''
+                    languages = list(script.Script.LangSysRecord)
+                    for language in languages:
+                        lang_tag = language.LangSysTag
+                        font_languages[font][lang_tag] = ''
 
-            for lang_tag in font_languages[font]:
-                try:
-                    db.connect()
-                    db_language = Languages.get(Languages.part3 == otlang2iso[lang_tag])
-                    font_languages[font][lang_tag] = db_language
-                    db.close()
-                except:
-                    all # Need to add other OpenType scripts to script2iso
+                for lang_tag in font_languages[font]:
+                    try:
+                        db.connect()
+                        db_language = Languages.get(Languages.part3 == otlang2iso[lang_tag])
+                        font_languages[font][lang_tag] = db_language
+                        db.close()
+                    except:
+                        all # Need to add other OpenType scripts to script2iso
+            except KeyError:
+                print('Skipping language tests, no GSUB table')
+                return None
         return font_languages

093871b9eed339a719c2f1db04accd23a2be2f2a
Marc Foley <m.foley.88@gmail.com>
2018-07-12 09:54:06 +0100

    refactored models.comparison
    The previous implementation produced deeply nested json which featured
    every diff result. This situation led to performance issues on comparisons
    which featured many differences (2k+).
    
    The new implementation saves each diff result individually. When a user loads
    a particular diff, only that data is returned. By saving each result individually,
    the data isn't as nested which has made the templates more readable

--- app/settings.py
+++ app/settings.py
@@ -2,4 +2,12 @@ import os
 
 GLYPH_AREA_THRESHOLD = 7000
 FONTS_DIR = os.path.join('static', 'fonts')
+VIEWS = (
+    'waterfall',
+    'glyphs_all', 'glyphs_new', 'glyphs_missing', 'glyphs_modified',
+    'marks_new', 'marks_missing', 'marks_modified',
+    'mkmks_new', 'mkmks_missing', 'mkmks_modified',
+    'kerns_new', 'kerns_missing', 'kerns_modified',
+    'metrics_modified',
+)
 DIFF_LIMIT = 800

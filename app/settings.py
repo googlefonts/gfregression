0a53aaccbda94b5dbf91aae342907ff98daef89e
Marc Foley <m.foley.88@gmail.com>
2018-10-19 16:20:54 +0100

    settings: DIFF_FAMILIES flag added.
    Sometimes diffing the families isn't needed. A user may just want to
    compare two fonts in a waterfall (for hinting purposes) or play with
    the editor.

--- app/settings.py
+++ app/settings.py
@@ -3,12 +3,16 @@ import os
 GLYPH_AREA_THRESHOLD = 7000
 FONTS_DIR = os.path.join('static', 'fonts')
 MEDIA_DIR = os.path.join('static', 'media')
-VIEWS = (
-    'waterfall',
-    'glyphs_all', 'glyphs_new', 'glyphs_missing', 'glyphs_modified',
-    'marks_new', 'marks_missing', 'marks_modified',
-    'mkmks_new', 'mkmks_missing', 'mkmks_modified',
-    'kerns_new', 'kerns_missing', 'kerns_modified',
-    'metrics_modified',
-)
+
+DIFF_FAMILIES = True
+if DIFF_FAMILIES:
+    VIEWS = [
+        'glyphs_all', 'glyphs_new', 'glyphs_missing', 'glyphs_modified',
+        'marks_new', 'marks_missing', 'marks_modified',
+        'mkmks_new', 'mkmks_missing', 'mkmks_modified',
+        'kerns_new', 'kerns_missing', 'kerns_modified',
+        'metrics_modified',
+    ]
+else:
+    VIEWS = []
 DIFF_LIMIT = 800
